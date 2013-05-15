39e5e4387bda9dfe122bb40a49c0449d5ed77dc3
David Johnson <djohnson@aspingroup.com.au>
2013-05-15 14:51:41 +1000

    Implemented save() on model instances.

--- test/integration.js
+++ test/integration.js
@@ -1,3 +1,29 @@
-describe('INTEGRATION', function() {
-	it("should work under all conditions");
+require('should');
+var _ = require('underscore');
+var Model = require('../lib/platos-model');
+
+describe('INTEGRATION', function () {
+	before(function () {
+		Model.connect('platos-model-test');
+	});
+	
+	it("Class.save() with no properties should save a document with only _id", function (done) {
+		var Class = Model.create('Class');
+		var instance = new Class();
+		
+		instance.save(function (err, document) {
+			_.isNull(err).should.be.ok;
+			_.size(document).should.equal(1);
+			document.should.have.property('_id');
+
+			done();
+		});
+		
+	});
+	
+	after(function (done) {
+		Model._db.dropDatabase(function () {
+			done();
+		});
+	});
 });

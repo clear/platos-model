71b1f8682bf3fa53ce0ec09b97a79bf5919dfabd
David N. Johnson <me@dnj.io>
2013-05-22 18:05:02 +1000

    Abstracted away resolution of tenant/non-tenant arguments. Added support for Model.update().

--- test/integration/update.js
+++ test/integration/update.js
@@ -3,12 +3,12 @@ var _ = require("underscore");
 var Platos = require("../../lib/platos-model");
 
 describe("INTEGRATION - UPDATE", function () {
+	var Model = Platos.create("Model");
 	var instance;
 	
 	beforeEach(function (done) {
 		//Insert one document into the database to test updating against
 		Platos._db.collection("Model").drop(function () {
-			var Model = Platos.create("Model");
 			instance = new Model();
 			instance.test = "property";
 
@@ -20,41 +20,66 @@ describe("INTEGRATION - UPDATE", function () {
 		Platos._db.collection("Model").drop(done);
 	});
 	
-	it("Model.save() second call should update document (_id will remain the same)", function (done) {
-		var _id = instance._id;
-		_id.should.be.ok;
+	describe("model.save()", function () {
+		it("second call should update document (_id will remain the same)", function (done) {
+			var _id = instance._id;
+			_id.should.be.ok;
 		
-		instance.save(function (err) {
-			_.isNull(err).should.be.ok;
-			instance._id.should.equal(_id);
+			instance.save(function (err) {
+				_.isNull(err).should.be.ok;
+				instance._id.should.equal(_id);
 			
-			done();
+				done();
+			});
+		});
+	
+		it("second call should update existing property", function (done) {
+			instance.test = "property2";
+		
+			instance.save(function () {
+				_.size(instance).should.equal(2);
+				instance.should.have.property("test");
+				instance.test.should.equal("property2");
+			
+				done();
+			});
+		});
+	
+		it("second call should keep existing property and add new", function (done) {
+			instance.test2 = "property2";
+		
+			instance.save(function () {
+				_.size(instance).should.equal(3);
+				instance.should.have.property("test");
+				instance.test.should.equal("property");
+				instance.should.have.property("test2");
+				instance.test2.should.equal("property2");
+			
+				done();
+			});
 		});
 	});
 	
-	it("Model.save() second call should update existing property", function (done) {
-		instance.test = "property2";
+	describe("Model.update()", function () {
+		it("should update document without inserting another", function (done) {
+			var _id = instance._id;
+			_id.should.be.ok;
 		
-		instance.save(function () {
-			_.size(instance).should.equal(2);
-			instance.should.have.property("test");
-			instance.test.should.equal("property2");
-			
-			done();
-		});
-	});
-	
-	it("Model.save() second call should keep existing property and add new", function (done) {
-		instance.test2 = "property2";
-		
-		instance.save(function () {
-			_.size(instance).should.equal(3);
-			instance.should.have.property("test");
-			instance.test.should.equal("property");
-			instance.should.have.property("test2");
-			instance.test2.should.equal("property2");
-			
-			done();
+			Model.update({ _id: _id }, { $set: { test2: "property2" } }, function (err) {
+				_.isNull(err).should.be.ok;
+				
+				//Re-find
+				Model.find(function (err, objects) {
+					_.isNull(err).should.be.ok;
+					_.isArray(objects).should.be.ok;
+					objects.length.should.equal(1);
+					objects[0]._id.equals(_id).should.be.ok;
+					objects[0].should.have.property("test");
+					objects[0].should.have.property("test2");
+					
+					done();
+				});
+			});
 		});
 	});
 });
